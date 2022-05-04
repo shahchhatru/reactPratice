d89ba35fa10ad57211175009c99df56e605e218e
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-05-04 15:33:58 +0545

    useRef example

--- src/App.js
+++ src/App.js
@@ -1,30 +1,21 @@
 
 import './App.css';
 import User from './User.js';
-import React,{useState,useEffect} from 'react';
+import React,{useState,useEffect,useRef} from 'react';
 
 function App(){
-  let [count,setcount]=useState(0);
-  let [index,setIndex]=useState(0);
-
-  useEffect(()=>{
-    console.log("count incremented");
-  },[count]);
-
-  useEffect(()=>{
-    console.log("index incremented");
-  },[index])
-
+  const refDiv =useRef();
+  const changeColor=(refDiv)=>{
+    let r=Math.floor(Math.random()*255+1);
+    let g=Math.floor(Math.random()*255+1);
+    let b=Math.floor(Math.random()*255+1);
+    let a=Math.floor(Math.random()*10)/10;
+    refDiv.current.style.backgroundColor="rgba("+r+","+g+","+b+","+a+")";
+  }
   return(
     <>
-    <h1>count {count }</h1>
-    <h2>index{index}</h2>
-    <div>
-      <button onClick={()=>{setcount(count+1)}}>Click Me</button>
-    </div>
-    <div>
-      <button onClick={()=>{setIndex(index+1)}}>index inc</button>
-    </div>
+    <div class="box-container" ref={refDiv}></div>
+    <button onClick={()=>{changeColor(refDiv)}} >Click</button>
     </>
   )
 }
